dc678d1565b507c8d271d800fd3199a2106c3a28
bareenarashid <102651470+bareenarashid@users.noreply.github.com>
2022-03-30 12:56:44 +0530

    Template update

---
+++ eshop/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from eshop.models import categories, j_arrived, trandy
+# Register your models here.
+admin.site.register(categories)
+admin.site.register(trandy)
+admin.site.register(j_arrived)
